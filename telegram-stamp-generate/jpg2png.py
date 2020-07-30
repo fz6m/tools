1cc4f62362f28963f889d545e9dc4c9c96b1b3f3
fz6m <fz6m@qq.com>
2020-07-30 18:26:54 +0800

    update telegram jpg2png

--- telegram-stamp-generate/jpg2png.py
+++ telegram-stamp-generate/jpg2png.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 # 图片和该脚本放在一个文件夹内启动
+# 可以处理 .jpg 与 .png 批量转换为 Telegram 贴图格式
 
 import os
 from PIL import Image
@@ -8,12 +9,17 @@ from PIL import Image
 list = os.listdir()
 
 for i in list:
-    if i.find('.py') == -1:
+    suffix = i[i.rfind('.') + 1:]
+    if suffix == 'jpg' or suffix == 'png':
         try:
             print('开始处理 ' + i)
+            dir = './out'
+            saveFileName = dir + '/' + i[:i.find('.')] + '.png'
+            if not os.path.exists(dir):
+                os.mkdir(dir)
             img = Image.open(i).convert('RGBA')
             if img.size[0] == img.size[1]:
-                img.resize((512, 512)).save(i[:i.find('.')] + '.png')
+                img.resize((512, 512)).save(saveFileName)
             else:
                 basemap = Image.new(mode='RGBA', size=(512, 512))
                 if img.size[0] > img.size[1]:
@@ -26,7 +32,7 @@ for i in list:
                     basemap.paste(img.resize((int(adjustWidth), 512)),
                                 (int((512 - adjustWidth) / 2), 0),
                                 img.resize((int(adjustWidth), 512)))
-                basemap.save(i[:i.find('.')] + '.png')
+                basemap.save(saveFileName)
         except:
             print(i + ' 出错')
 
